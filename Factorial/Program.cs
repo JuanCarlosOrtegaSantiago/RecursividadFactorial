e6209d90dda499eef465a254d49e8e1c69304ee5
JuanCarlosOrtegaSantiago <37173001+JuanCarlosOrtegaSantiago@users.noreply.github.com>
2018-09-03 23:33:33 -0500

    Programa de recursividad Programa de estructura de datos que muestra la recursividad con el ejemplo de recursividad

---
+++ Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int valor;
+                Console.WriteLine("Ingresa el numero:");
+                valor = int.Parse(Console.ReadLine());
+                Recursivo r = new Recursivo();
+                Console.WriteLine("El factorial de {0} es: {1}",valor,r.Fac(valor));
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error en: {0}",ex);
+            }
+            
+
+        }
+    }
+}
